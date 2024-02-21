2e2617eaebc54322f45f41117f5585f996bb7084
Evan <stevanuspangau50@gmail.com>
2024-02-21 17:26:30 +0700

    Char

---
+++ src/main/kotlin/3.Char.kt
@@ -0,0 +1,23 @@
+fun main(){
+//    contoh deklarasi char
+    var character = 'A'
+    val character2: Char = 'B'
+
+//    contoh operasi aritmatika pada char
+    var vocal = 'A'
+    println(vocal++)
+    println(vocal++)
+    println(vocal++)
+    println(vocal--)
+    println(vocal--)
+    println(vocal--)
+    println(vocal--)
+
+    /*
+    * Operasi increment dan decrement sendiri merupakan operasi yang bisa kita gunakan pada tipe data Number.
+    * Lalu kenapa kita bisa menggunakannya pada tipe Char?
+    * Karena pada dasarnya setiap Characters merupakan representasi dari Unicode.
+    * Contoh Unicode A adalah 0041. Ketika kita melakukan increment maka hasilnya adalah 0042
+    * yang mana merupakan Unicode dari B.
+    * */
+}
