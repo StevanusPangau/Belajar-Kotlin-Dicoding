4308bd255a321708cd11de2699442faf0695d483
Evan <stevanuspangau50@gmail.com>
2024-02-21 17:56:46 +0700

    Numbers

---
+++ src/main/kotlin/7.Boolean.kt
@@ -0,0 +1,36 @@
+fun main(){
+//    Conjunction atau AND
+    val officeOpen = 7
+    val officeClosed = 16
+    val now = 20
+
+    val isOpen = if (now >= officeOpen && now <= officeClosed){
+        true
+    } else {
+        false
+    }
+
+    println("Office is open : $isOpen")
+
+    /*
+    * Output : Office is open : false
+    * */
+
+//    Disjunction atau OR
+    val isClose = now < officeOpen || now > officeClosed
+
+    println("Office is closed : $isClose")
+    /*
+    * Output : Office is closed : true
+    * */
+
+//    Negation atau NOT
+    if(!isOpen){
+        println("Office is closed")
+    } else {
+        println("Office is open")
+    }
+    /*
+    * Output : Office is open : false
+    * */
+}
