06082631168dc64636aa157122ef43836bf78a22
Evan <stevanuspangau50@gmail.com>
2024-02-21 20:24:38 +0700

    Nullable

---
+++ src/main/kotlin/10.Nullable.kt
@@ -0,0 +1,24 @@
+fun main() {
+//    Contoh error ketika nullable
+//    val name: String = null // Error
+
+//    Contoh nullable
+    val name: String? = null
+
+//    Tapi nullable tidak bisa di berikan value langsung, jadinya kita harus menggunakan if else
+//    val name: String? = "Kotlin" // Error
+
+    //Smart cast
+    var name2: String? = "Kotlin"
+    if (name2 != null) {
+        println(name2)
+        println(name2.length)
+    }
+
+//    Is atau !is
+    var name3: Any? = "Evan"
+    if (name3 is String) {
+        // Tidak membutuhkan casting secara eksplisit.
+        println("String length is ${name3.length}")
+    }
+}
