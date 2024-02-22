6bf3c145f8eca78ae2e0751e963be4aff070c28a
Evan <stevanuspangau50@gmail.com>
2024-02-22 14:53:26 +0700

    while dan do while

---
+++ src/main/kotlin/ControlFlow/3.While_and_do_while.kt
@@ -0,0 +1,17 @@
+package ControlFlow
+
+fun main(){
+//    while loop
+    var counter = 1
+    while (counter <= 7){
+        println("While Loop!")
+        counter++
+    }
+
+//    do while loop
+    var counter2 = 1
+    do {
+        println("Do While Loop!")
+        counter2++
+    } while (counter2 <= 7)
+}
